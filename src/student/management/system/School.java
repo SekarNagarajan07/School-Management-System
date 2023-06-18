3ab0ffc65bdf30e0bdaf305276e63c38a0c0a975
sekar N <sekarnagarajan1234@gmail.com>
2023-06-18 19:42:11 +0530

    java mini project

--- src/student/management/system/School.java
+++ src/student/management/system/School.java
@@ -1,5 +1,22 @@
 package student.management.system;
 
-public class School {
+import java.util.List;
 
+/**
+ * Many teachers, many students
+ * Implements teachers and student
+ * Using an ArrayList
+ */
+public class School {
+    private List <Teacher> teachers;
+    private List <Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
 }
