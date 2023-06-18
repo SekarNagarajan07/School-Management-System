40092e0578269d7cb9ad4ad449a8d913423d5f1b
sekar N <sekarnagarajan1234@gmail.com>
2023-06-18 19:50:33 +0530

    java mini project

--- src/student/management/system/School.java
+++ src/student/management/system/School.java
@@ -19,4 +19,36 @@ public class School {
         totalMoneyEarned = 0;
         totalMoneySpent = 0;
     }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public void setTotalMoneyEarned(int totalMoneyEarned) {
+        this.totalMoneyEarned = totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public void setTotalMoneySpent(int totalMoneySpent) {
+        this.totalMoneySpent = totalMoneySpent;
+    }
 }
